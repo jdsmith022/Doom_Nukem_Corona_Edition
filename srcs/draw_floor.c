badd23824d68bcb1652f44d1e7545a3365f9b63c
Jessica Smith <jessicasmith@jessicadsmith.com>
2020-05-13 12:26:00 +0200

    saving for checking out

--- srcs/draw_floor.c
+++ srcs/draw_floor.c
@@ -6,7 +6,7 @@
 /*   By: Malou <Malou@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/15 13:43:16 by Malou         #+#    #+#                 */
-/*   Updated: 2020/05/13 11:58:08 by jessicasmit   ########   odam.nl         */
+/*   Updated: 2020/05/13 12:25:51 by jessicasmit   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
