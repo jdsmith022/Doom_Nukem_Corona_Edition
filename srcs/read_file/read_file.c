45300b38176a173eb9f128cb257ebe98119f5291
Jessica Smith <jessicasmith@jessicadsmith.com>
2020-07-01 15:46:30 +0200

    working textures with SDL

--- srcs/read_file/read_file.c
+++ srcs/read_file/read_file.c
@@ -1,45 +1,74 @@
-#include "../../includes/doom.h"
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   read_file.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jessicasmith <jessicasmith@student.coda      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/07/01 15:43:35 by jessicasmit   #+#    #+#                 */
+/*   Updated: 2020/07/01 15:45:16 by jessicasmit   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
-t_sector  *save_sectors(int fd, int *len)
+#include "../../includes/doom.h"
+
+t_sector  		*save_sectors(int fd, int *len)
 {
 	t_sector    *sect;
 	char        *line;
 
-	get_line(&line, fd, "the amount of sectors is not specified or can not be read", 1);
+	get_line(&line, fd,\
+		"the amount of sectors is not specified or can not be read", 1);
 	*len = ft_atoi(line);
 	sect = (t_sector*)ft_memalloc(sizeof(t_sector) * *len);
 	free(line);
 	return (sect);
 }
 
-t_sidedef  *save_walls(int fd)
+t_sidedef  			*save_walls(int fd)
 {
 	t_sidedef   *walls;
 	char        *line;
 	int         len;
 
-	get_line(&line, fd, "the amount of walls is not specified or can not be read", 1);
+	get_line(&line, fd,\
+		"the amount of walls is not specified or can not be read", 1);
 	len = ft_atoi(line);
 	walls = (t_sidedef*)ft_memalloc(sizeof(t_sidedef) * len);
 	free(line);
 	return (walls);
 }
 
-t_object  *save_sprites(int fd)
+t_object  			*save_sprites(int fd)
 {
 	t_object   *sprites;
 	char        *line;
 	int         len;
 
-	get_line(&line, fd, "the amount of stationary sprites is not specified or can not be read", 1);
+	get_line(&line, fd,\
+		"the amount of stationary sprites is not specified or can not be read",\
+		1);
 	len = ft_atoi(line);
 	sprites = (t_object*)ft_memalloc(sizeof(t_object) * len);
 	free(line);
 	return (sprites);
 }
 
-static SDL_Surface    **save_img_array(t_bmp *images, char *line, int map_fd, int len)
+static void			save_bpm_to_sdl(t_bmp *images,
+						SDL_Surface **lib, int index)
+{
+	lib[index] = SDL_CreateRGBSurface(0, \
+		images[index].info.width, images[index].info.height,\
+		24, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
+	if (lib[index] == NULL)
+		doom_exit_lib_failure(images, MALLOC_ERR);
+	ft_memcpy(lib[index]->pixels, images[index].pixels,\
+		(images[index].info.img_size));
+	// print_meta_data(images[index].info);
+}
+
+static SDL_Surface	**read_from_line(t_bmp *images, char *line,
+						int map_fd, int len)
 {
 	SDL_Surface **lib;
 	int         fd;
@@ -59,14 +88,7 @@ static SDL_Surface    **save_img_array(t_bmp *images, char *line, int map_fd, in
 			continue ;
 		}
 		images[index] = read_bmp(fd);
-		lib[index] = SDL_CreateRGBSurface(0, \
-			images[index].info.width, images[index].info.height,\
-			24, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
-		if (lib[index] == NULL)
-			doom_exit_lib_failure(images, MALLOC_ERR);
-		ft_memcpy(lib[index]->pixels, images[index].pixels,\
-			(images[index].info.img_size));
-		print_meta_data(images[index].info);
+		save_bpm_to_sdl(images, lib, index);
 		free(line);
 		index++;
 	}
@@ -79,10 +101,11 @@ SDL_Surface         **save_img(int map_fd)
 	char        *line;
 	int         len;
 
-	get_line(&line, map_fd, "the amount of textures is not specified or can not be read", 1);
+	get_line(&line, map_fd,\
+		"the amount of textures is not specified or can not be read", 1);
 	len = ft_atoi(line);
 	images = (t_bmp *)ft_memalloc(sizeof(t_bmp) * len);
 	if (images == NULL)
 		doom_exit_failure(NULL, MALLOC_ERR);
-	return (save_img_array(images, line, map_fd, len));
+	return (read_from_line(images, line, map_fd, len));
 }
