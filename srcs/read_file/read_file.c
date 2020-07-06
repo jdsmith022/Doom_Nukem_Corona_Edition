4626bc774ee482724093b7af6d8cb3dacb1050e9
Jessica Smith <jessicasmith@jessicadsmith.com>
2020-07-06 17:37:17 +0200

    updated

--- srcs/read_file/read_file.c
+++ srcs/read_file/read_file.c
@@ -5,13 +5,8 @@
 /*                                                     +:+                    */
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
-<<<<<<< HEAD
-/*   Created: 2020/07/01 15:43:35 by jessicasmit   #+#    #+#                 */
-/*   Updated: 2020/07/06 16:00:57 by jessicasmit   ########   odam.nl         */
-=======
 /*   Created: 2020/07/05 15:35:26 by jesmith       #+#    #+#                 */
-/*   Updated: 2020/07/05 15:39:52 by jesmith       ########   odam.nl         */
->>>>>>> 7c713f7f1155caa1db223035468954427aaed9bf
+/*   Updated: 2020/07/06 17:31:55 by jessicasmit   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,69 +52,4 @@ t_object			*save_sprites(int fd)
 	sprites = (t_object*)ft_memalloc(sizeof(t_object) * len);
 	free(line);
 	return (sprites);
-}
-
-static void			save_bpm_to_sdl(t_bmp *images,
-						SDL_Surface **lib, int index)
-{
-	lib[index] = SDL_CreateRGBSurface(0, \
-		images[index].info.width, images[index].info.height,\
-		24, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
-	if (lib[index] == NULL)
-		doom_exit_lib_failure(images, MALLOC_ERR);
-	ft_memcpy(lib[index]->pixels, images[index].pixels,\
-		(images[index].info.img_size));
-	// print_meta_data(images[index].info);
-}
-
-static SDL_Surface	**read_from_line(t_bmp *images, char *line,
-						int map_fd, int len)
-{
-	SDL_Surface **lib;
-	int			fd;
-	int			index;
-
-	index = 0;
-	lib = (SDL_Surface**)ft_memalloc(sizeof(SDL_Surface*) * len);
-	if (lib == NULL)
-		doom_exit_lib_failure(images, MALLOC_ERR);
-	while (index < len)
-	{
-		get_line(&line, map_fd, "not enough texture names", 0);
-		fd = open(line, O_RDONLY);
-		if (fd < 0)
-		{
-			index++;
-			continue ;
-		}
-		images[index] = read_bmp(fd);
-		save_bpm_to_sdl(images, lib, index);
-		free(line);
-		index++;
-	}
-	return (lib);
-}
-
-// SDL_Surface			**save_sky(void)
-// {
-// 	char	*line;
-// 	int		sky_fd;
-
-// 	sky_fd
-// 	get_line()
-// }
-
-SDL_Surface			**save_img(int map_fd)
-{
-	t_bmp		*images;
-	char		*line;
-	int			len;
-
-	get_line(&line, map_fd,\
-		"the amount of textures is not specified or can not be read", 1);
-	len = ft_atoi(line);
-	images = (t_bmp *)ft_memalloc(sizeof(t_bmp) * len);
-	if (images == NULL)
-		doom_exit_failure(NULL, MALLOC_ERR);
-	return (read_from_line(images, line, map_fd, len));
 }
--- srcs/render/draw_skybox.c
+++ srcs/render/draw_skybox.c
@@ -6,14 +6,14 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/05 11:14:16 by jesmith       #+#    #+#                 */
-/*   Updated: 2020/07/06 16:18:16 by jessicasmit   ########   odam.nl         */
+/*   Updated: 2020/07/06 15:43:40 by jessicasmit   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/doom.h"
 
 static void		put_sky(t_doom *doom, Uint32 tex_dex, Uint32 index,
-					Uint32 pixel_dex)
+					Uint64 pixel_dex)
 {
 	char	*pixels;
 	char	*texture;
@@ -32,24 +32,19 @@ static void		put_sky(t_doom *doom, Uint32 tex_dex, Uint32 index,
 static void		draw_side_textures(t_doom *doom, t_plane plane,
 					t_sidedef sidedef, t_point pixel, Uint32 tex_dex)
 {
-	Uint32	pixel_dex;
+	Uint64	pixel_dex;
 	Uint32	index;
 	double	wall_y;
 	int		bpp;
 
 	bpp = doom->surface->format->BytesPerPixel;
-	// pixel.x += doom->pos.x;
-	// pixel.y += doom->pos.y;
-	// pixel.x = (int)pixel.x % doom->lib.sky_lib[tex_dex]->w;
-	// pixel.y = (int)pixel.y % doom->lib.sky_lib[tex_dex]->h;
 	index = (pixel.y * doom->surface->pitch) + (int)(pixel.x * bpp);
 	wall_y = (double)(doom->texture_height / plane.height_standard) *\
-		((int)(pixel.y + plane.wall_offset) - plane.sidedef_top);
+		((int)(pixel.y + plane.wall_offset) - plane.sidedef_top) * 8;
 	bpp = doom->lib.sky_lib[tex_dex]->format->BytesPerPixel;
 	pixel_dex = ((int)wall_y * doom->lib.sky_lib[tex_dex]->pitch) +\
-		((int)sidedef.offset * bpp);
+		((int)sidedef.offset * bpp) * 8;
 	put_sky(doom, tex_dex, index, pixel_dex);
-	//put_dimishing_lighting(&texure[pixel_dex], sidedef.distance)
 }
 
 static void		find_side(t_doom *doom, int x, t_sidedef sidedef,
@@ -58,116 +53,39 @@ static void		find_side(t_doom *doom, int x, t_sidedef sidedef,
 	t_point	pixel;
 	t_point	start;
 	t_point end;
+	Uint32	tex_dex;
 	double	dir_angle;
 
 	pixel.x = x;
 	pixel.y = plane.sidedef_top;
-	start = sidedef.line.start;
-	end = sidedef.line.end;
-	// printf("doom->ray_dir_angle: %f\n", doom->dir_angle);
-		printf("sidedef.dir= %d\n", sidedef.dir);
+	printf("sidedef.dir= %d	pos: %f, %f intersect: %f, %f\n", sidedef.dir, doom->pos.x, doom->pos.y, plane.intersect.x, plane.intersect.y);
 	while (pixel.y <= plane.sidedef_bottom)
 	{
 		dir_angle = doom->dir_angle;
-		// dir_angle *= PI / 180;
-		if (dir_angle >= 0 && dir_angle <= PI / 2 && sidedef.dir == 1)
-			draw_side_textures(doom, plane, sidedef, pixel, 2);
-		else if (dir_angle > PI / 2 && dir_angle < PI && sidedef.dir == 0)
-			draw_side_textures(doom, plane, sidedef, pixel, 3);
-		else if (dir_angle >= PI && dir_angle <= (3 * PI) / 2 && sidedef.dir == 1)
-			draw_side_textures(doom, plane, sidedef, pixel, 1);
-		else if (dir_angle > (3 * PI) / 2 && dir_angle < 2 * PI && sidedef.dir == 0)
-			draw_side_textures(doom, plane, sidedef, pixel, 5);
+		if (plane.intersect.x > doom->pos.x && sidedef.dir == 0)
+			tex_dex = 0;
+		else if (plane.intersect.x < doom->pos.x && sidedef.dir == 0)
+			tex_dex = 2;
+		else if (plane.intersect.y > doom->pos.y && sidedef.dir == 1)
+			tex_dex = 3;
+		else
+			tex_dex = 4;
+		draw_side_textures(doom, plane, sidedef, pixel, tex_dex);
 		pixel.y++;
 	}
 }
 
-static void		put_row(t_doom *doom, Uint32 tex_dex,
-					Uint32 index, Uint64 pixel_dex)
-{
-	char *pixels;
-	char *texture;
 
-	pixels = doom->surface->pixels;
-	texture = doom->lib.sky_lib[tex_dex]->pixels;
-	pixels[index] = texture[pixel_dex];
-	index++;
-	pixel_dex++;
-	pixels[index] = texture[pixel_dex];
-	index++;
-	pixel_dex++;
-	pixels[index] = texture[pixel_dex];
-}
-
-static void		row_calculations(t_doom *doom, double dist, Uint32 index,
-					Uint32 tex_dex)
-{
-	t_point	texture;
-	t_point	floor;
-	Uint64	pixel_dex;
-	Uint8	bpp;
-
-	bpp = doom->lib.sky_lib[tex_dex]->format->BytesPerPixel;
-	floor.x = dist * cos(doom->ray_angle);
-	floor.y = dist * sin(doom->ray_angle);
-	// floor.x -= doom->pos.x;
-	// floor.y -= doom->pos.y;
-	texture.x = (int)floor.x % doom->lib.sky_lib[tex_dex]->w;
-	texture.y = (int)floor.y % doom->lib.sky_lib[tex_dex]->h;
-	pixel_dex = (((int)texture.y * doom->lib.sky_lib[tex_dex]->pitch)\
-		+ ((int)texture.x * bpp));
-	put_row(doom, tex_dex, index, pixel_dex);
-}
-
-void			draw_ground(t_doom *doom, int x,
-					t_sector sector, int y)
-{
-	double	dist;
-	Uint32	index;
-	Uint32	height;
-	Uint32	tex_dex;
-	Uint8	bpp;
-
-	tex_dex = 1;
-	height = HEIGHT / 2;
-	bpp = doom->surface->format->BytesPerPixel;
-	while (y < HEIGHT)
-	{
-		index = (y * doom->surface->pitch) + (x * bpp);
-		dist = doom->player_std_height / (y - height) * (doom->dist_to_plane);
-		dist /= cos(doom->ray_adjacent * x - FOV / 2);
-		row_calculations(doom, dist, index, tex_dex);
-		y++;
-	}
-}
-
-static void		draw_sky(t_doom *doom, int x, t_sector sector, int y)
-{
-	double	dist;
-	Uint32	index;
-	Uint32	tex_dex;
-	Uint32	height;
-	Uint8	bpp;
-
-	tex_dex = 5;
-	bpp = doom->surface->format->BytesPerPixel;
-	height = HEIGHT / 2;
-	while (y >= 0)
-	{
-		index = (y * doom->surface->pitch) + (x * bpp);
-		dist = doom->player_std_height / (height - y) * doom->dist_to_plane;
-		dist /= cos(doom->ray_adjacent * x - FOV / 2);
-		row_calculations(doom, dist, index, tex_dex);
-		y--;
-	}
-}
 
 void	draw_skybox(t_doom *doom, int x, t_sidedef sidedef, t_plane plane)
 {
-	t_sector sector;
+	t_sector	sector;
+	t_point		center;
 
 	sector = doom->lib.sector[sidedef.sector];
+
 	draw_sky(doom, x, sector, plane.sidedef_top);
 	find_side(doom, x, sidedef, plane);
 	draw_ground(doom, x, sector, plane.sidedef_bottom);
+	// printf("finished\n");
 }
