bf5878024559314bba62f941dd57a487e0dc8c0f
Jessica Smith <jessicasmith@jessicadsmith.com>
2020-07-07 15:17:27 +0200

    saved lib ft_rounder properly

--- libft/ft_rounder.c
+++ libft/ft_rounder.c
@@ -6,13 +6,13 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/05 14:34:28 by jesmith       #+#    #+#                 */
-/*   Updated: 2020/07/05 14:35:42 by jesmith       ########   odam.nl         */
+/*   Updated: 2020/07/07 15:16:36 by jessicasmit   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int     ft_rounded(double dbl)
+int     ft_rounder(double dbl)
 {
     return ((int)(dbl + 0.5));
 }
