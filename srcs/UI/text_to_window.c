d5a5c1c8309736a57ec67863e4d36770bedfe20f
Jessica Smith <jessicasmith@jessicadsmith.com>
2020-07-08 13:14:13 +0200

    saving
    need to make font surface connected in doom strcut

--- srcs/UI/text_to_window.c
+++ srcs/UI/text_to_window.c
@@ -6,13 +6,22 @@
 /*   By: jessicasmith <jessicasmith@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/03 15:16:05 by jessicasmit   #+#    #+#                 */
-/*   Updated: 2020/07/08 12:20:13 by jessicasmit   ########   odam.nl         */
+/*   Updated: 2020/07/08 13:13:24 by jessicasmit   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/doom.h"
 
+static void fontInit(t_doom *doom)
+{
+        TTF_Init();
+        font = TTF_OpenFont("text/amatic/AmaticSC-Regular.tff", 12);
+        fColor.r = 255;
+        fColor.g = 255;
+        fColor.b = 255;
+}
+
 void        draw_text(t_doom *doom)
 {
-    
+    fontInit(doom);
 }
