86db09d107ad43bcdee899721cb62e8ceff75ef7
Jessica D. Smith <jesmith@student.codam.nl>
2020-07-21 12:32:11 +0200

    fixed ground and sky

--- srcs/render/draw_skybox_top_bottom.c
+++ srcs/render/draw_skybox_top_bottom.c
@@ -26,12 +26,12 @@ static void		row_calculations(t_doom *doom, double dist, Uint32 index,
 	Uint8	bpp;
 
 	bpp = doom->lib.sky_lib[tex_dex]->format->BytesPerPixel;
-	floor.x = dist * cos(doom->ray_angle) / 2;
-	floor.y = dist * sin(doom->ray_angle) / 2;
-	floor.x += 32;
-	floor.y += 32;
-	texture.x = ((int)floor.x % doom->texture_width) * 2;
-	texture.y = ((int)floor.y % doom->texture_width) * 2;
+	floor.x = dist * cos(doom->ray_angle);
+	floor.y = dist * sin(doom->ray_angle);
+	floor.x += 32 * 2;
+	floor.y += 32 * 2;
+	texture.x = ((int)floor.x % doom->texture_width);
+	texture.y = ((int)floor.y % doom->texture_width);
 	pixel_dex = ((int)texture.y * doom->lib.sky_lib[tex_dex]->pitch)\
 		+ ((int)texture.x * bpp);
 	put_row(doom, tex_dex, index, pixel_dex);
