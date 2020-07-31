bfb918feff14d55023d729a9870387ef0fdddd4b
Fre <fre@Fres-iMac.local>
2020-07-31 10:29:14 +0200

    fixed it

--- srcs/gameplay/add_infection.c
+++ srcs/gameplay/add_infection.c
@@ -4,8 +4,7 @@
 
 void    init_infection(t_sidedef *infection, t_doom *doom)
 {
-    int x;
-    int y;
+    int change;
     int i;
     int x_diff;
     int y_diff;
@@ -15,43 +14,16 @@ void    init_infection(t_sidedef *infection, t_doom *doom)
     i = -1;
     while (i == -1 || doom->lib.sidedef[i].opp_sector != -1)
         i = rand() % doom->lib.len_sidedef;
-    x_diff = doom->lib.sidedef[i].line.end.x > doom->lib.sidedef[i].line.start.x ?
-        (doom->lib.sidedef[i].line.end.x) - (doom->lib.sidedef[i].line.start.x) :
-        (doom->lib.sidedef[i].line.start.x) - (doom->lib.sidedef[i].line.end.x);
-	y_diff = doom->lib.sidedef[i].line.end.y > doom->lib.sidedef[i].line.start.y ?
-        (doom->lib.sidedef[i].line.end.y) - (doom->lib.sidedef[i].line.start.y) :
-        (doom->lib.sidedef[i].line.start.y) - (doom->lib.sidedef[i].line.end.y);
+    x_diff = (doom->lib.sidedef[i].line.end.x) - (doom->lib.sidedef[i].line.start.x);
+	y_diff = (doom->lib.sidedef[i].line.end.y) - (doom->lib.sidedef[i].line.start.y);
 	x_steps = (float)x_diff / (float)(abs(x_diff) + abs(y_diff));
 	y_steps = (float)y_diff / (float)(abs(x_diff) + abs(y_diff));
-    if (x_diff == 0)
-        x_diff++;
-    if (y_diff == 0)
-        y_diff++;
-    // infection->line.start.x = doom->lib.sidedef[i].line.end.x > doom->lib.sidedef[i].line.start.x ? 
-    //                             rand() % x_diff + doom->lib.sidedef[i].line.start.x :
-    //                             rand() % x_diff + doom->lib.sidedef[i].line.end.x;
-    // y = doom->lib.sidedef[i].line.end.x > doom->lib.sidedef[i].line.start.x ?
-    //     infection->line.start.x - doom->lib.sidedef[i].line.start.x :
-    //     infection->line.start.x - doom->lib.sidedef[i].line.end.x;
-
-    // infection->line.start.y = (y - y * x_steps) / x_steps;
-    // infection->line.start.y =  doom->lib.sidedef[i].line.end.y > doom->lib.sidedef[i].line.start.y ? 
-    //     (y * y_steps) +  doom->lib.sidedef[i].line.start.y :
-    //     (y * y_steps) +  doom->lib.sidedef[i].line.end.y;
-    // infection->line.start.y = doom->lib.sidedef[i].line.end.y > doom->lib.sidedef[i].line.start.y ? 
-    //                             rand() % (y_diff - (int)(y_steps * 16)) + doom->lib.sidedef[i].line.start.y :
-    //                             rand() % (y_diff - (int)(y_steps * 16)) + doom->lib.sidedef[i].line.end.y;
-    int change;
     if ((abs(x_diff) + abs(y_diff)) != 0)
         change = rand() % (abs(x_diff) + abs(y_diff));
     else
         change = 1;
-    infection->line.start.x = doom->lib.sidedef[i].line.end.x > doom->lib.sidedef[i].line.start.x ? 
-                                change * x_steps + doom->lib.sidedef[i].line.start.x :
-                                change * x_steps + doom->lib.sidedef[i].line.end.x;
-    infection->line.start.y = doom->lib.sidedef[i].line.end.y > doom->lib.sidedef[i].line.start.y ? 
-                                change * y_steps + doom->lib.sidedef[i].line.start.y :
-                                change * y_steps + doom->lib.sidedef[i].line.end.y;
+    infection->line.start.x = change * x_steps + doom->lib.sidedef[i].line.start.x;
+    infection->line.start.y = change * y_steps + doom->lib.sidedef[i].line.start.y;
     infection->line.end.x = (infection->line.start.x + x_steps * 15);
     infection->line.end.y = (infection->line.start.y + y_steps * 15);
 	infection->id = 0;
