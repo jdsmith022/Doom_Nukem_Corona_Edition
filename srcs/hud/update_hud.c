5be9d1f1fb8c4038605133971e789d3f1d57bfd0
Jessica Smith <JessicaSmith@Jessies-MacBook-Pro.local>
2020-08-04 20:22:28 +0200

    new hud

--- srcs/hud/update_hud.c
+++ srcs/hud/update_hud.c
@@ -1,5 +1,37 @@
 #include "../../includes/doom.h"
 
+// static void	draw_hud_top_bar(t_doom *doom)
+// {
+// 	Uint32	*pixels;
+// 	int		x;
+// 	int		y;
+
+// 	x = 0;
+// 	pixels = doom->surface->pixels;
+// 	while (x < WIDTH)
+// 	{
+// 		y = HEIGHT - 55;
+// 		while (y < 100)
+// 		{
+// 			if (y >= 100 - 55 && y < HEIGHT - 53 && \
+// 			x >= 0 && x <= WIDTH && y >= 0 && y <= HEIGHT)
+// 				pixels[(y * WIDTH + x)] = 0x00000;
+// 			if (y > HEIGHT - 53)
+// 				printf("here\n");
+// 			else if (y > HEIGHT - 53 && HEIGHT < HEIGHT - 49)
+// 			{
+// 				printf("here\n");
+// 				pixels[(y * WIDTH + x)] = 0x2C2C2E;
+// 			}
+// 			else if ( y > HEIGHT - 49 && \
+// 			 x >= 0 && x <= WIDTH && y >= 0 && y <= HEIGHT)
+// 				pixels[(y * WIDTH + x)] = 0x00002E;
+// 			y++;
+// 		}
+// 		x++;
+// 	}
+// }
+
 static void	draw_hud_bottom_bar(t_doom *doom)
 {
 	Uint32	*pixels;
@@ -16,13 +48,11 @@ static void	draw_hud_bottom_bar(t_doom *doom)
 			if (y >= HEIGHT - 55 && y < HEIGHT - 53 && \
 			x >= 0 && x <= WIDTH && y >= 0 && y <= HEIGHT)
 				pixels[(y * WIDTH + x)] = 0x00000;
-			else if (y > HEIGHT - 53 && HEIGHT < HEIGHT - 49)
-			{
-				printf("here\n");
-				pixels[(y * WIDTH + x)] = 0x616169;
-			}
-			else if ( y > HEIGHT - 49 && \
-			 x >= 0 && x <= WIDTH && y >= 0 && y <= HEIGHT)
+			else if (y >= HEIGHT - 53 && y < HEIGHT - 49 &&\
+			x >= 0 && x <= WIDTH && y >= 0 && y <= HEIGHT)
+				pixels[(y * WIDTH + x)] = 0x2C2C2E;
+			else if (y >= HEIGHT - 49 && \
+			x >= 0 && x <= WIDTH && y >= 0 && y <= HEIGHT)
 				pixels[(y * WIDTH + x)] = 0x00002E;
 			y++;
 		}
