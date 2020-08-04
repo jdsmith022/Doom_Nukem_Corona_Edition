8d3a2119728cb7cd38ed2ad463fbee75afe1c1cd
Jessica Smith <JessicaSmith@Jessies-MacBook-Pro.local>
2020-08-04 20:16:22 +0200

    saving hud

--- srcs/hud/update_hud.c
+++ srcs/hud/update_hud.c
@@ -1,26 +1,29 @@
 #include "../../includes/doom.h"
 
-static void	draw_hud_background(t_doom *doom)
+static void	draw_hud_bottom_bar(t_doom *doom)
 {
-	int x;
-	int y;
-	Uint32 color;
 	Uint32	*pixels;
-	Uint32	limit;
+	int		x;
+	int		y;
 
 	x = 0;
-	limit = WIDTH / 4;
-	color = 0x00002E;
 	pixels = doom->surface->pixels;
 	while (x < WIDTH)
 	{
-		y = HEIGHT - 60;
-		while (x >= limit && x <= limit * 3)
-			x++;
-		while (y < HEIGHT - 10)
+		y = HEIGHT - 55;
+		while (y < HEIGHT)
 		{
-			if (x >= 0 && x <= WIDTH && y >= 0 && y <= HEIGHT)
-				pixels[(y * WIDTH + x)] = color;
+			if (y >= HEIGHT - 55 && y < HEIGHT - 53 && \
+			x >= 0 && x <= WIDTH && y >= 0 && y <= HEIGHT)
+				pixels[(y * WIDTH + x)] = 0x00000;
+			else if (y > HEIGHT - 53 && HEIGHT < HEIGHT - 49)
+			{
+				printf("here\n");
+				pixels[(y * WIDTH + x)] = 0x616169;
+			}
+			else if ( y > HEIGHT - 49 && \
+			 x >= 0 && x <= WIDTH && y >= 0 && y <= HEIGHT)
+				pixels[(y * WIDTH + x)] = 0x00002E;
 			y++;
 		}
 		x++;
@@ -30,6 +33,9 @@ static void	draw_hud_background(t_doom *doom)
 void		update_hud(t_doom *doom)
 {
 	if (doom->game_editor == FALSE)
-		draw_hud_background(doom);
+	{
+		// draw_hud_bottom_top(doom);
+		draw_hud_bottom_bar(doom);
+	}
 	font_to_screen(doom);
 }
