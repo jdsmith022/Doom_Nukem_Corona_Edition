ad37a574e103e3f0642ca52c7638a3092e6c0d12
Roos Cocolien <r.steigenga@gmail.com>
2020-08-04 17:11:51 +0200

    added scissorlift

--- srcs/sprite/sprite_draw.c
+++ srcs/sprite/sprite_draw.c
@@ -47,18 +47,20 @@ int		find_tex_x(t_doom *doom, t_point *sprite_begin, t_point *sprite_end,\
 	return (tex_x);
 }
 
-int		find_tex_y(t_doom *doom, t_point *sprite_begin, t_point *sprite_end, int index_sp, int screen_y)
+int		find_tex_y(t_doom *doom, t_point *sprite_begin, t_point *sprite_end,\
+		int index_sp, int screen_y)
 {
 	int		i_sprite;
 	int		tex_y;
 
-	// i_sprite = doom->lib.sprites[index_sp].index;
 	i_sprite = doom->lib.sprites[index_sp].visible; //multiple faces
 	tex_y = 0;
 	if (sprite_begin->y > 0 && sprite_begin->y < HEIGHT)
-		tex_y = (int)(screen_y - sprite_begin->y) / doom->lib.sprites[index_sp].height * doom->lib.obj_lib[i_sprite]->h;
+		tex_y = (int)(screen_y - sprite_begin->y) /\
+		doom->lib.sprites[index_sp].height * doom->lib.obj_lib[i_sprite]->h;
 	else
-		tex_y = (int)(sprite_end->y - screen_y) / doom->lib.sprites[index_sp].height * doom->lib.obj_lib[i_sprite]->h;
+		tex_y = (int)(sprite_end->y - screen_y) /\
+		doom->lib.sprites[index_sp].height * doom->lib.obj_lib[i_sprite]->h;
 	return (tex_y);
 }
 
@@ -67,11 +69,8 @@ int		no_clipping_region(int screen_y, t_sprite sprite, t_doom *doom, int index_s
 	int		i;
 
 	i = 0;
-	if (sprite.n_sector > 0)
-		// printf("sprite.n_sector: %d, sprite #%d\n", sprite.n_sector, index_sp);
 	while (i < sprite.n_sector)
 	{
-		// printf("i: %d, prev_sector[i]: %d, stripe: %d\n", i, sprite.prev_sectors[i], stripe);
 		if (doom->lib.sector[sprite.prev_sectors[i]].sidedef_bottom[stripe] > 0 &&\
 		doom->lib.sector[sprite.prev_sectors[i]].sidedef_bottom[stripe] < HEIGHT &&\
 		doom->lib.sector[sprite.prev_sectors[i]].sidedef_bottom[stripe] < screen_y)
@@ -81,7 +80,8 @@ int		no_clipping_region(int screen_y, t_sprite sprite, t_doom *doom, int index_s
 	return (1);
 }
 
-void	draw_stripes(t_doom *doom, t_point *sprite_begin, t_point *sprite_end, int index_sp)
+void	draw_stripes(t_doom *doom, t_point *sprite_begin, t_point *sprite_end,\
+		int index_sp)
 {
 	int			i_sprite;
 	Uint32		index;
@@ -103,12 +103,15 @@ void	draw_stripes(t_doom *doom, t_point *sprite_begin, t_point *sprite_end, int 
 		{
 			screen_y = (int)sprite_begin->y;
 			tex_x = find_tex_x(doom, sprite_begin, sprite_end, index_sp, stripe);
-			while (screen_y < (int)sprite_end->y  && no_clipping_region(screen_y, sprite, doom,\
-			index_sp, stripe) == 1)
+			while (screen_y < (int)sprite_end->y  && no_clipping_region(screen_y,\
+			sprite, doom, index_sp, stripe) == 1)
 			{
-				index = (size_t)(screen_y * doom->surface->pitch) + (int)(stripe * doom->surface->format->BytesPerPixel);
-				tex_y = find_tex_y(doom, sprite_begin, sprite_end, index_sp, screen_y);
-				pix_dex = ((int)tex_y * doom->lib.obj_lib[i_sprite]->pitch) + ((int)tex_x * doom->lib.obj_lib[i_sprite]->format->BytesPerPixel);
+				index = (size_t)(screen_y * doom->surface->pitch) +\
+				(int)(stripe * doom->surface->format->BytesPerPixel);
+				tex_y = find_tex_y(doom, sprite_begin, sprite_end,\
+				index_sp, screen_y);
+				pix_dex = ((int)tex_y * doom->lib.obj_lib[i_sprite]->pitch)\
+				+ ((int)tex_x * doom->lib.obj_lib[i_sprite]->format->BytesPerPixel);
 				put_pixel_tex(doom, pix_dex, index, i_sprite);
 				screen_y++;
 			}
