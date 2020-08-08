5caecd49420e5e061a8bbdf199e82e9aa21a6a00
Jessica D. Smith <jesmith@student.codam.nl>
2020-08-08 19:00:35 +0200

    saving health levels

--- srcs/hud/calculate_hud_levels.c
+++ srcs/hud/calculate_hud_levels.c
@@ -5,14 +5,12 @@ static void		update_hud_levels_2(t_doom *doom)
 {
 	if (doom->hud->shopper == TRUE)
 	{
-		printf("shopper\n");
 		doom->hud->corona_level += 10;
 		doom->hud->shopper = FALSE;
 	}
 	else if (doom->hud->corona == TRUE)
 	{
 		doom->hud->corona_level += 15;
-		printf("corona level: %d\n", doom->hud->corona_level);
 		doom->hud->corona = FALSE;
 	}
 }
@@ -31,19 +29,16 @@ void		calculate_hud_levels(t_doom *doom)
 	}
 	else if (doom->hud->facemask == TRUE)
 	{
-		printf("facemask\n");
 		doom->hud->corona_level -= 10;
 		doom->hud->facemask = FALSE;
 	}
 	else if (doom->hud->health_pack == TRUE)
 	{
-		printf("health_pack\n");
 		doom->hud->corona_level -= 5;
 		doom->hud->health_pack = FALSE;
 	}
 	else if (doom->hud->health_pack_plus == TRUE)
 	{
-		printf("health_pack_plus\n");
 		doom->hud->corona_level -= 15;
 		doom->hud->health_pack_plus = FALSE;
 	}
--- srcs/hud/update_level.c
+++ srcs/hud/update_level.c
@@ -11,6 +11,8 @@ void	update_hud_levels(t_doom *doom, t_font *lib)
 	int		level;
 	char	*corona_level;
 	char	*cat;
+	char	*font_path;
+	int		font_size;
 
 	level = doom->hud->corona_level;
 	corona_level = ft_itoa(level);
@@ -32,4 +34,7 @@ void	update_hud_levels(t_doom *doom, t_font *lib)
 		lib[5].font_color = doom->lib.font_lib.font_color.red;
 	else
 		lib[5].font_color = doom->lib.font_lib.font_color.white;
+	font_path = "srcs/font/font_style/JosefinSans-Bold.ttf";
+	font_size = 22;
+	font_to_sdl(doom, doom->lib.font_lib.hud_font, font_path, font_size);
 }
