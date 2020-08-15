65d96965c0d0a53a5a827c2a7493e3b9f5a15504
Manon Louise <malou.minkjan@gmail.com>
2020-08-15 17:34:40 +0200

    slopes, but you can not really walk on them

--- srcs/render/set_properties_plane.c
+++ srcs/render/set_properties_plane.c
@@ -10,14 +10,14 @@ void		set_properties_plane_portal(t_doom *doom, t_sidedef sidedef,
 	int			new_height;
 
 	if (opp_sector->slope_floor_id != -1)
-		slope_plane_settings(doom, sidedef, opp_sector, 1);
+		slope_plane_settings(doom, sidedef, opp_sector);
 	new_height = (HEIGHT / 2) + doom->player_height;
 	height_ceiling = opp_sector->height_ceiling / \
 		sidedef.distance * doom->dist_to_plane;
 	mid_top = ((new_height - (plane->height_standard / 2)) - \
 		doom->own_event.y_pitch - height_ceiling) + 2;
 	plane->mid_texture_top = ((mid_top >= 0) ? mid_top : 0);
-	height_floor = (opp_sector->height_floor) / \
+	height_floor = opp_sector->height_floor / \
 		sidedef.distance * doom->dist_to_plane;
 	mid_bottom = ((new_height + (plane->height_standard / 2)) - \
 		doom->own_event.y_pitch - height_floor) - 2;
@@ -34,7 +34,7 @@ void		set_properties_plane_sidedef(t_doom *doom, t_sidedef sidedef,
 	int			new_height;
 
 	if (sector->slope_floor_id != -1)
-		slope_plane_settings(doom, sidedef, sector, 0);
+		slope_plane_settings(doom, sidedef, sector);
 	new_height = (HEIGHT / 2) + doom->player_height;
 	plane->height_standard = doom->texture_height / \
 		sidedef.distance * doom->dist_to_plane;
@@ -43,7 +43,7 @@ void		set_properties_plane_sidedef(t_doom *doom, t_sidedef sidedef,
 	sidedef_top = ((new_height - (plane->height_standard / 2)) - \
 		doom->own_event.y_pitch - height_ceiling);
 	wall_offset(plane, sidedef_top);
-	height_floor = (sector->height_floor) / \
+	height_floor = sector->height_floor / \
 		sidedef.distance * doom->dist_to_plane;
 	sidedef_bottom = ((new_height + (plane->height_standard / 2)) - \
 		doom->own_event.y_pitch - height_floor);
