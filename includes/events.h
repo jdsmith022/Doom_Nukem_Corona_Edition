10df7862c019d2dfbe8dc32c6ebff51699c18956
Jessica D. Smith <jesmith@student.codam.nl>
2020-08-22 09:08:47 +0200

    changed text box to have tinted background instead of white

---
+++ includes/events.h
@@ -0,0 +1,27 @@
+#ifndef EVENTS_H
+# define EVENTS_H
+
+# include "doom.h"
+# include "struct.h"
+
+/*events functions*/
+void				key_press(t_doom *doom, t_event *event,\
+						SDL_KeyboardEvent *key);
+void				key_release(t_event *event, SDL_KeyboardEvent *key);
+void				key_handler(t_doom *doom, t_event *event, double dt);
+void				mouse_press(t_doom *doom,\
+						SDL_MouseButtonEvent *button, t_event event);
+void				mouse_release(t_doom *doom,\
+						SDL_MouseButtonEvent *button);
+void				camera_movement(t_doom *doom,\
+						SDL_MouseMotionEvent *motion, double dt);
+void				move_cam_direction(t_doom *doom,\
+						SDL_MouseMotionEvent *motion,\
+						double dt, t_event *event);
+void				set_new_position(t_doom *doom, t_event *event, double dt);
+int					check_floor_diff(t_doom *doom, int sector, int next_sector);
+int					check_sector_height_diff(t_doom *doom,\
+						int sector, int next_sector);
+bool				handle_mouse_state(t_doom *doom);
+
+#endif
