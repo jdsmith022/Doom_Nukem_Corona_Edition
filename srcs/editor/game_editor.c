2a6f5acbb6a2e59dc616ba3084fb876f61dec7c5
Jessica Smith <jessicasmith@jessicadsmith.com>
2020-08-28 12:15:09 +0200

    saving for merge

--- srcs/editor/game_editor.c
+++ srcs/editor/game_editor.c
@@ -1,6 +1,8 @@
 #include "../../includes/doom.h"
 #include "game_editor.h"
 
+#include <stdio.h>
+
 void	open_game_editor(t_doom *doom)
 {
 	Uint32				*pixels;
@@ -12,20 +14,19 @@ void	open_game_editor(t_doom *doom)
 	SDL_SetRelativeMouseMode(SDL_FALSE);
 	doom->distance = 1;
 	pixels = doom->surface->pixels;
-	if (doom->lib.sector == NULL)
+	if (SECTOR == NULL)
 		init_game_design(doom);
 	draw_screen_colors(pixels, doom);
 	draw_images(pixels, doom);
 	bars(&pixels, doom);
-	i = doom->lib.sector[doom->game_design.cur_sec].i_sidedefs;
-	while (i < doom->lib.sector[doom->game_design.cur_sec].i_sidedefs\
-		+ doom->lib.sector[doom->game_design.cur_sec].n_sidedefs)
+	i = SECTOR[EDIT.cur_sec].i_sidedefs;
+	while (i < SECTOR[EDIT.cur_sec].i_sidedefs\
+		+ SECTOR[EDIT.cur_sec].n_sidedefs)
 	{
 		draw_lines(doom, &pixels, i);
 		i++;
 	}
 	draw_object(doom, &pixels);
 	clock_gettime(doom->game_time, &curr_time);
-	doom->game_start_time.tv_sec = \
-	doom->game_start_time.tv_sec + (curr_time.tv_sec - hold_time.tv_sec);
+	doom->game_start_time.tv_sec = doom->game_start_time.tv_sec + (curr_time.tv_sec - hold_time.tv_sec);
 }
