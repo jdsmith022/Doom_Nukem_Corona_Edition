26f1524e87b5e1a4715a1c51ce5ee4d18a8d2328
Jessica D. Smith <jesmith@student.codam.nl>
2020-09-06 17:00:18 +0200

    removed two leaks from init_groceries

--- srcs/gameplay/init_groceries.c
+++ srcs/gameplay/init_groceries.c
@@ -1,14 +1,7 @@
 #include "../../includes/doom.h"
 #include "../../includes/gameplay.h"
 
-static void		set_grocery_font(TTF_Font **font)
-{
-	if (*font)
-		return ;
-	*font = TTF_OpenFont("srcs/font/font_style/Cicle_Semi.ttf", 16);
-}
-
-static uint8_t	get_num_of_groceries(t_doom *doom)
+uint8_t			get_num_of_groceries(t_doom *doom)
 {
 	uint16_t	i;
 	uint8_t		num_of_groceries;
@@ -24,26 +17,22 @@ static uint8_t	get_num_of_groceries(t_doom *doom)
 	return (num_of_groceries);
 }
 
-static void			init_default_groc(t_doom *doom)
+void			init_groceries(t_doom *doom)
 {
+	uint8_t		num_of_groceries;
+	int			i;
+
+	i = 0;
 	doom->groceries = (t_groceries *)ft_memalloc(sizeof(t_groceries));
 	if (doom->groceries == NULL)
 		doom_exit_failure(doom, "error: groceries malloc");
 	doom->groceries->num_of_groceries = get_num_of_groceries(doom);
-}
-
-void			init_groceries(t_doom *doom)
-{
-	uint8_t		num_of_groceries;
-
-	if (!doom->game_design.custom_level)
-		init_default_groc(doom);
-	num_of_groceries = doom->groceries->num_of_groceries;
 	if (doom->groceries->num_of_groceries < MAX_SL_LEN)
 		doom->groceries->shopping_list_len = num_of_groceries;
 	else
 		doom->groceries->shopping_list_len = MAX_SL_LEN;
-	generate_shopping_list(doom, doom->groceries->shopping_list);
+	generate_shopping_list(doom);
 	doom->groceries->basket = NULL;
-	set_grocery_font(&doom->groceries->font);
+	doom->groceries->tp = FALSE;
+	doom->groceries->font = doom->lib.font_lib.font_16;
 }
