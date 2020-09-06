4cff57a43b62ffc447c1de16ec71f81b664d3834
Jessica D. Smith <jesmith@student.codam.nl>
2020-09-06 12:04:41 +0200

    normed free library sdl

--- srcs/core/free_library_sdl.c
+++ srcs/core/free_library_sdl.c
@@ -6,7 +6,7 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/31 17:45:29 by jesmith       #+#    #+#                 */
-/*   Updated: 2020/09/06 12:03:10 by jesmith       ########   odam.nl         */
+/*   Updated: 2020/09/06 12:04:33 by jesmith       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,16 @@ static void		free_sld_libs(SDL_Surface **lib, int len)
 		index++;
 	}
 	free(lib);
+}
+
+static void		free_sdl_lib_2(t_doom *doom)
+{
+	if (doom->lib.player_lib)
+	{
+		lib = doom->lib.player_lib;
+		len = doom->lib.len_player_lib;
+		free_sld_libs(lib, len);
+	}
 }
 
 void			free_sdl_lib(t_doom *doom)
@@ -49,10 +59,6 @@ void			free_sdl_lib(t_doom *doom)
 		len = doom->lib.len_sky_lib;
 		free_sld_libs(lib, len);
 	}
-	if (doom->lib.player_lib)
-	{
-		lib = doom->lib.player_lib;
-		len = doom->lib.len_player_lib;
-		free_sld_libs(lib, len);
-	}
+	else
+		free_sdl_lib_2(doom);
 }
