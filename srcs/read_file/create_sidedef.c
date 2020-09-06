cb955ce17d8e4ded1fdb5ceb4e7d0d646d24a3b6
Neli marinde De wild <nde-wild@f1r4s10.codam.nl>
2020-09-06 19:36:18 +0200

    fix to the i_sidedef problem

--- srcs/read_file/create_sidedef.c
+++ srcs/read_file/create_sidedef.c
@@ -6,7 +6,7 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/25 10:43:52 by jesmith       #+#    #+#                 */
-/*   Updated: 2020/08/25 10:43:53 by jesmith       ########   odam.nl         */
+/*   Updated: 2020/09/06 19:32:04 by nde-wild      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,12 @@ static void		moving_sidedef(t_doom *doom, t_sidedef *sidedef, int k, int i)
 	doom->lib.sector[i].n_sidedefs++;
 }
 
-static void		set_col_lib(t_doom *doom, int *k, int *total_sd_index, int i)
+static void		set_col_lib(t_doom *doom, int k, int *total_sd_index, int i)
 {
-	if (!*k)
-	{
-		*k = 0;
-		*total_sd_index = 0;
-	}
-	else
+
+	if (k != 0)
 		*total_sd_index += doom->lib.sector[i].i_sidedefs;
-	doom->lib.sector[i].i_sidedefs = *total_sd_index;
+	doom->lib.sector[i].i_sidedefs = k;
 }
 
 static void		set_sidedef_slope(t_doom *doom, int k, int i)
@@ -47,8 +43,13 @@ void			create_sidedef(t_doom *doom, int fd, int len,
 	static int	total_sd_index;
 	int			sidedef_in_sector;
 
+	if (!k)
+	{
+		k = 0;
+		total_sd_index = 0;
+	}
 	doom->lib.sector[i] = sector_inf(doom, fd, doom->lib.len_tex_lib);
-	set_col_lib(doom, &k, &total_sd_index, i);
+	set_col_lib(doom, k, &total_sd_index, i);
 	sidedef_in_sector = 0;
 	while (sidedef_in_sector < doom->lib.sector[i].n_sidedefs)
 	{
