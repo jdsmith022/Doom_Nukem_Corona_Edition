8d274caeb9c6ce1547098ca8e22f416611590f50
Jessica Smith <JessicaSmith@Jessies-MacBook-Pro.local>
2020-09-07 22:38:07 +0200

    normed files but
    gameplay and audio

--- srcs/render/add_light_to_pixel.c
+++ srcs/render/add_light_to_pixel.c
@@ -6,7 +6,7 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/29 14:00:59 by jesmith       #+#    #+#                 */
-/*   Updated: 2020/09/06 23:48:18 by JessicaSmit   ########   odam.nl         */
+/*   Updated: 2020/09/07 22:36:24 by JessicaSmit   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,4 @@ void		add_light_to_pixel(t_doom *doom, t_sector sector, int x, int y)
 	}
 	else
 		add_light_to_pixel_2(doom, sector, x, y);
-	 
 }
