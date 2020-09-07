11aff79dfa3e5da71951461c4a5651589ff40add
Jessica Smith <JessicaSmith@Jessies-MacBook-Pro.local>
2020-09-07 10:02:36 +0200

    saving save_libraries

--- srcs/read_file/save_libraries.c
+++ srcs/read_file/save_libraries.c
@@ -6,7 +6,7 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/31 17:45:20 by jesmith       #+#    #+#                 */
-/*   Updated: 2020/09/06 23:42:29 by JessicaSmit   ########   odam.nl         */
+/*   Updated: 2020/09/07 10:02:01 by JessicaSmit   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ int			open_file(t_doom *doom, char *filename)
 
 void		modified(t_doom *doom, char *file_name)
 {
-	// struct stat filestat;
+	struct stat filestat;
 
-	// stat(file_name, &filestat);
-	// if ((long long)filestat.st_mtime != (long long)filestat.st_birthtime)
-	// 	doom_exit_failure(doom, "error: file has been modified");
+	stat(file_name, &filestat);
+	if ((long long)filestat.st_mtime != (long long)filestat.st_birthtime)
+		doom_exit_failure(doom, "error: file has been modified");
 }
 
 void		save_libraries(t_doom *doom)
