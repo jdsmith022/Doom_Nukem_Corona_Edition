65f2325d3ab856baf5cbc9c5bd87e216a16a96f7
Jessica Smith <JessicaSmith@Jessies-MacBook-Pro.local>
2020-09-11 15:01:17 +0200

    added modified to player sprites

--- srcs/read_file/save_player_settings_sprites.c
+++ srcs/read_file/save_player_settings_sprites.c
@@ -6,7 +6,7 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/06 11:52:36 by jesmith       #+#    #+#                 */
-/*   Updated: 2020/09/06 20:23:28 by jesmith       ########   odam.nl         */
+/*   Updated: 2020/09/11 14:44:40 by JessicaSmit   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ void					save_player_sprites(t_doom *doom)
 {
 	int fd;
 
+	modified(doom, "srcs/read_file/player_sprites");
 	fd = open_file(doom, "srcs/read_file/player_sprites");
 	doom->lib.player_lib = save_objects(doom, fd, &doom->lib.len_player_lib);
 }
